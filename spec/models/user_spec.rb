ecff819f65a167901edf97e348474eea690bc0c5
bennypaulino <santiagopro71@gmail.com>
2017-04-07 18:48:58 -0400

    Adjust specs to align with :username attribute

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe User do
   describe 'validations' do
-    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:username) }
     it { should validate_presence_of(:email) }
     it { should validate_presence_of(:password) }
   end
