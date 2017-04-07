1d6b1fada4fb8542e9be500cef8a740b9a97b6fb
bennypaulino <santiagopro71@gmail.com>
2017-04-07 18:18:45 -0400

    Write spec/models/user_spec.rb
    * Add devise gem

--- spec/features/account_creation_feature_spec.rb
+++ spec/features/account_creation_feature_spec.rb
@@ -5,10 +5,10 @@ describe 'account creation' do
     visit root_path
     click_link 'Create Account'
 
-    # fill_in 'Name', with: 'Indiana'
-    # fill_in 'Email', with: 'indy@jones.com'
-    # fill_in 'Password', with: 'templeofdoom'
-    # fill_in 'Password Confirmation', with: 'templeofdoom'
+    fill_in 'Name', with: 'Indiana'
+    fill_in 'Email', with: 'indy@jones.com'
+    fill_in 'Password', with: 'templeofdoom'
+    fill_in 'Password Confirmation', with: 'templeofdoom'
     fill_in 'Subdomain', with: 'test_subdomain'
     click_button 'Create Account'
 
