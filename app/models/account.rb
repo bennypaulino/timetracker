d78f48df739538e468ac7a7084c2bdec6b5e9bf3
bennypaulino <santiagopro71@gmail.com>
2017-04-07 21:40:34 -0400

    Ensure feature tests pass

--- app/models/account.rb
+++ app/models/account.rb
@@ -9,6 +9,8 @@ class Account < ActiveRecord::Base
                         format: { with: /\A[\w\-]+\Z/i, message: 'contains invalid characters' },
                         exclusion: { in: RESTRICTED_SUBDOMAINS, message: 'restricted' }
 
+  accepts_nested_attributes_for :owner
+
   before_validation :downcase_subdomain
 
   private
