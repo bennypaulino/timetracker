66a2ff6c9640988f209583b326fa62e42a385d97
bennypaulino <santiagopro71@gmail.com>
2017-04-24 13:34:12 -0400

    Provide seed for apartment

--- config/initializers/apartment.rb
+++ config/initializers/apartment.rb
@@ -1,5 +1,5 @@
 Apartment.configure do |config|
   config.seed_after_create = true
-  # config.excluded_models = ['Account']
+  config.excluded_models = ['Account']
   config.tenant_names = -> { Account.pluck(:subdomain) }
 end
